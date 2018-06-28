3478f3e52ea4692d35436a711874e34d672334fa
Gia1987 <giacomo.n27@gmail.com>
2018-06-28 22:51:04 +0100

    Create getRecipe method and connect to Form.js by props

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Form from './components/Form';
 
 class App extends Component {
+  getRecipe = (e) => {
+    const recipeName = e.target.elements.recipeName.value;
+    e.preventDefault();
+    console.log(recipeName)
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Recipe Search</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Form getRecipe={this.getRecipe} />
       </div>
     );
   }
