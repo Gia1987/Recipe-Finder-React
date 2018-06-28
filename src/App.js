786d1d09a834dd13410b11324cbb07074059ae58
Gia1987 <giacomo.n27@gmail.com>
2018-06-29 00:07:27 +0100

    Added API to App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import React, { Component } from 'react';
 import './App.css';
 import Form from './components/Form';
 
+const API_KEY = 'aedbb2d845263a9cad4857bcec585195';
+
 class App extends Component {
-  getRecipe = (e) => {
+
+  getRecipe = async(e) => {
     const recipeName = e.target.elements.recipeName.value;
     e.preventDefault();
-    console.log(recipeName)
+    const api_call = await fetch(`https://cors-anywhere.herokuapp.com/http://food2fork.com/api/search?key=${API_KEY}&q=shredded%20chicken`);
+    const data = await api_call.json();
+    console.log(data.recipes[0].publisher);
   }
 
   render() {
